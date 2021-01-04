ae649dd90c84c790849e2e6ac91a872eeeebcaf8
Ethan Green <egreen18@rams.colostate.edu>
2021-01-03 23:10:31 -0700

    Changing structure format

--- AbilityVal.m
+++ AbilityVal.m
@@ -16,27 +16,31 @@ if l < 1
 elseif l > 5
     disp("This ability has an invalid level.")
 else
-    for j = 1:length(champdat.(champ.ch).abilities.(key).effects(1).leveling)
-        effect(j).value = 0;
-        effect(j).att = champdat.(champ.ch).abilities.(key).effects(1).leveling(j).attribute;
-        abi = champdat.(champ.ch).abilities.(key).effects(1).leveling(j).modifiers;
-        for i = 1:length(abi)
-            unit = abi(i).units{l};
-            switch unit
-                case '' %Base damage
-                    effect(j).value = effect(j).value + abi(i).values(l);
-                case '% AD' %AD ratio
-                    effect(j).value = effect(j).value + abi(i).values(l)*champ.stats.attackDamage/100;
-                case '% AP' %AP ratio
-                    effect(j).value = effect(j).value + abi(i).values(l)*champ.stats.abilityPower/100;
-                case '% MS'  %Percent movespeed
-                    effect(j).value = abi(i).values(l)*champ.stats.movespeed/100;
-                case '% bonus AD' %Bonus AD ratio
-                    effect(j).value = effect(j).value + abi(i).values(l)*(champ.stats.attackDamage...
-                        - champ.sta_base.attackDamage)/100;
-                case '% attack speed' %Total AS ratio - modifier of AD ratio
-                    effect(j).value = effect(j).value + abi(i).values(l)*champ.stats.attackSpeed*...
-                        (champ.stats.attackDamage - champ.sta_base.attackDamage)/100;
+    for k = 1:length(champdat.(champ.ch).abilities.(key).effects)
+        if ~isempty(champdat.(champ.ch).abilities.(key).effects(k).leveling)
+            for j = 1:length(champdat.(champ.ch).abilities.(key).effects(k).leveling)
+                effect(j,k).value = 0;
+                effect(j,k).att = champdat.(champ.ch).abilities.(key).effects(k).leveling(j).attribute;
+                abi = champdat.(champ.ch).abilities.(key).effects(k).leveling(j).modifiers;
+                for i = 1:length(abi)
+                    unit = abi(i).units{l};
+                    switch unit
+                        case '' %Base damage
+                            effect(j,k).value = effect(j,k).value + abi(i).values(l);
+                        case '% AD' %AD ratio
+                            effect(j,k).value = effect(j,k).value + abi(i).values(l)*champ.stats.attackDamage/100;
+                        case '% AP' %AP ratio
+                            effect(j,k).value = effect(j,k).value + abi(i).values(l)*champ.stats.abilityPower/100;
+                        case '% MS'  %Percent movespeed
+                            effect(j,k).value = abi(i).values(l)*champ.stats.movespeed/100;
+                        case '% bonus AD' %Bonus AD ratio
+                            effect(j,k).value = effect(j,k).value + abi(i).values(l)*(champ.stats.attackDamage...
+                                - champ.sta_base.attackDamage)/100;
+                        case '% attack speed' %Total AS ratio - modifier of AD ratio
+                            effect(j,k).value = effect(j,k).value + abi(i).values(l)*champ.stats.attackSpeed*...
+                                (champ.stats.attackDamage - champ.sta_base.attackDamage)/100;
+                    end
+                end
             end
         end
     end
